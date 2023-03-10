68ac2856569c9d53bd4f9b385f00e20bfd4c5560
Lucas <kleijvander@gmail.com>
2023-03-10 21:33:58 +0100

    feat: sets global code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,13 @@ import { getJestProjects } from '@nrwl/jest';
 
 export default {
   projects: getJestProjects(),
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
