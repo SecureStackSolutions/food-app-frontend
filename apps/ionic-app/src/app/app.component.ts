70ac1e448d4307362cf244f3c4a9ab5f0d61b0ea
Lucas <kleijvander@gmail.com>
2023-03-10 17:17:17 +0100

    feat: adds empty ionic app

---
+++ apps/ionic-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ionic-template-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+}
