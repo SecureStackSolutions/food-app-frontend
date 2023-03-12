c3579500145e1e0d423d67482e71f322b3a79253
Lucas <kleijvander@gmail.com>
2023-03-12 21:26:06 +0100

    feat: adds tailwind css and authentication feature

---
+++ apps/main-app/src/app/libs/features/authentication/authentication.feature.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'feature-authentication',
+   templateUrl: 'authentication.feature.html',
+   styleUrls: ['authentication.feature.scss'],
+   standalone: true,
+})
+export class AuthenticationFeature {}
--- apps/main-app/src/app/libs/pages/authentication/authentication.module.ts
+++ apps/main-app/src/app/libs/pages/authentication/authentication.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { AuthenticationFeature } from '../../features';
 import { AuthenticationPage } from './authentication.page';
 import { AuthenticationPageRoutingModule } from './authentication.routing';
 
 @NgModule({
-   imports: [CommonModule, IonicModule, AuthenticationPageRoutingModule],
+   imports: [CommonModule, IonicModule, AuthenticationPageRoutingModule, AuthenticationFeature],
    declarations: [AuthenticationPage],
 })
 export class AuthenticationPageModule {}
