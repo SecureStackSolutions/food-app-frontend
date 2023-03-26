f2d4f1f0c2e1e07c479a892f42875380ed122a5f
Lucas <kleijvander@gmail.com>
2023-03-26 20:18:03 +0200

    minor update

--- apps/main-app/src/app/libs/shared/functions/convertWindowThemeToAppTheme.ts
+++ apps/main-app/src/app/libs/shared/functions/convertWindowThemeToAppTheme.ts
@@ -2,6 +2,5 @@ import { AppTheme } from '../../state-management';
 
 export function convertWindowThemeToAppTheme(): AppTheme {
    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-   const prefersLight = window.matchMedia('(prefers-color-scheme: light)');
    return prefersDark.matches ? 'dark' : 'light';
 }
