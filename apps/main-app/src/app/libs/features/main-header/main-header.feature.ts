dd981250beb6bcd68ed06442c1f002ad369ff340
Lucas <kleijvander@gmail.com>
2023-03-27 21:15:21 +0200

    moves tab bar to left on wide screens

--- apps/main-app/src/app/libs/features/main-header/main-header.feature.ts
+++ apps/main-app/src/app/libs/features/main-header/main-header.feature.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
    selector: 'feature-main-header',
    templateUrl: 'main-header.feature.html',
    styleUrls: ['main-header.feature.scss'],
 })
-export class MainHeaderFeature {}
+export class MainHeaderFeature {
+   @Input() titleCollapsed: string;
+}
